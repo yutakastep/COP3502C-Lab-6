684a8dc885a27c93a39ca90341f26e9cf09cddfa
Armin Anton <arminantondm@gmail.com>
2023-03-08 05:01:15 +0000

    added decoder

--- encoder.py
+++ encoder.py
@@ -2,9 +2,7 @@
 # Author: Yutaka Stephens
 # Partner: Guilherme Armin Da Silva Anton
 
-def encode():
-    # input for the password
-    pass_input = input('Please enter your password to encode: ')
+def encode(pass_input):
     # turns input string into a list to iterate through
     password = list(pass_input.strip())
     # turns list values into an integer, and then adds 3 to each
@@ -13,10 +11,19 @@ def encode():
     encoded_pass = ''.join(str(i) for i in new_pass)
     return encoded_pass
 
+def decode(original):
+    chars = [] # new list/array
+    chars[:] = original # splits the string into individual chars in a list/array
+    encoded = "" # string that will contain the encoded password
+    
+    for char in chars: # loop through the items of the list, one char at the time
+        num = int(char) # converts the char to int
+        num += 3 # adds 3 to it
 
-def decode():
-    pass
+        # uses f string to convert int to string and concatenates it to whatever was within the string variable
+        encoded += f"{num}"
 
+    return encoded
 
 def menu():
     # pretty straightforward, just using a function to simplify the menu code in main
@@ -27,16 +34,33 @@ def menu():
     input_option = int(input('Please enter an option: '))
     return input_option
 
+def main():
+    # store pass and encoded pass
+    src_pass = ""
+    enc_pass = ""
 
-if __name__ == "__main__":
-    option = menu()
-    while option != 3:
+    while True:
+        option = menu()
         if option == 1:
-            enc_pass = encode()
+            src_pass = input('Please enter your password to encode: ')
+            enc_pass = encode(src_pass)
             print('Your password has been encoded and stored!')
-            print()
-            option = menu()
+
         elif option == 2:
-            print()
-    else:
-        pass
+            if enc_pass == "": # if no source pass was provided
+                src_pass = input("Please enter your password to decode: ")
+            enc_pass = decode(src_pass)
+            print(f"The encoded password is {enc_pass}, and the original password is {src_pass}.")
+
+        elif option == 3: # exit code
+            break
+
+        else:
+            print("Invalid option")
+
+        print()
+
+# only executes if encoder.py is the main caller file
+# does not execute if loaded as a module
+if __name__ == "__main__":
+    main()
